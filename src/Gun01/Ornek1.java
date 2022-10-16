081f5db2e5a163cb013a19220204ece471b74e0d
alkan <alkannarin1986@gmail.com>
2022-10-16 07:15:05 +0300

    3. Örnek

--- src/Gun01/Ornek1.java
+++ src/Gun01/Ornek1.java
@@ -4,5 +4,7 @@ public class Ornek1 {
     public static void main(String[] args) {
         System.out.println("merhaba dünya");
         System.out.println("merhaba 2");
+
+        System.out.println("merhaba 3");
     }
 }
